6de2f1be2a3fb3e4c7fd6c9fad1c4c1efd88fdc0
Shubham Sinha <64971616+Cosmic-Infinity@users.noreply.github.com>
2024-12-04 10:23:20 +0530

    Sped up loop() by global declaration

--- Arduino/Sweep.ino
+++ Arduino/Sweep.ino
@@ -1,6 +1,7 @@
 #include <Servo.h>
 
 Servo myservo;  // create Servo object to control a servo
+int value;
 
 void setup() {
   myservo.attach(9);           // attaches the servo on pin 9 to the Servo object
@@ -11,7 +12,7 @@ void setup() {
 void loop() {
   // Check if any data is available on the Serial port
   if (Serial.available()) {
-    int value = Serial.parseInt(); // Read the integer value from Serial
+    value = Serial.parseInt(); // Read the integer value from Serial
     if (value >= 0 && value <= 180) { // Ensure value is within servo range
       myservo.write(value);          // Move servo to specified position
       //Serial.print("Servo position set to: ");
