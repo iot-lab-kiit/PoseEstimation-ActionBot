75e5b298ef0238cad3e9cc18905e2a325dc815e7
Shubham Sinha <64971616+Cosmic-Infinity@users.noreply.github.com>
2024-12-04 10:21:10 +0530

    Fixed crash

--- Arduino/Sweep.ino
+++ Arduino/Sweep.ino
@@ -14,11 +14,11 @@ void loop() {
     int value = Serial.parseInt(); // Read the integer value from Serial
     if (value >= 0 && value <= 180) { // Ensure value is within servo range
       myservo.write(value);          // Move servo to specified position
-      Serial.print("Servo position set to: ");
-      Serial.println(value);         // Provide feedback
+      //Serial.print("Servo position set to: ");
+      //Serial.println(value);         // Provide feedback
       digitalWrite(LED_BUILTIN, HIGH); // Turn on the LED
     } else {
-      Serial.println("Invalid position! Enter a value between 0 and 180.");
+      //Serial.println("Invalid position! Enter a value between 0 and 180.");
     }
   } else {
     digitalWrite(LED_BUILTIN, LOW); // Turn off the LED if no data
